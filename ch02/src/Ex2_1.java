0133af82113fc9e7bbaf5c7c9633e35a05153ff6
ldw0123 <ldw0123@gmail.com>
2022-02-01 22:24:19 +0900

    220201
    자바의 정석

---
+++ ch02/src/Ex2_1.java
@@ -0,0 +1,11 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// ctrl + alt + down : 행 단위 복사
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
